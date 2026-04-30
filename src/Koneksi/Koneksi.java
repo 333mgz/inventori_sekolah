c45dfccdfa80701d24f03a367da17ac5946857ec
333mgz <137768858+333mgz@users.noreply.github.com>
2026-04-30 17:01:29 +0700

    initial commit

---
+++ src/Koneksi/Koneksi.java
@@ -0,0 +1,30 @@
+package Koneksi;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Koneksi {
+
+    private static Connection conn;
+
+    public static Connection getKoneksi() {
+        try {
+            if (conn == null) {
+                String url = "jdbc:mysql://localhost:3306/inventori_sekolah?useSSL=false&serverTimezone=UTC";
+                String user = "root";
+                String pass = "";
+
+                Class.forName("com.mysql.jdbc.Driver");
+                conn = DriverManager.getConnection(url, user, pass);
+
+                System.out.println("Koneksi berhasil!");
+            }
+        } catch (Exception e) {
+            System.out.println("Koneksi gagal: " + e.getMessage());
+        }
+        return conn;
+    }
+}
+
